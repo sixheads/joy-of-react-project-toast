6b08296f1cb61f3146501124c4d9403b1b7fd37f
John Fry <john@sixheads.com>
2023-09-27 13:43:49 +1000

    Add event listener on escape key - exercise 5

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -44,6 +44,20 @@ function ToastProvider({ children }) {
     setToasts(nextToasts);
   }
 
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === 'Escape') {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <ToastContext.Provider
       value={{
